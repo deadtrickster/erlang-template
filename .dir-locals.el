73f4dd96a0463b4a29aef14b596465890d575bff
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-20 11:43:54 +0300

    first commit

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil         . ((indent-tabs-mode             . nil)
+                 (fill-column                  . 80)
+                 (eval                         . (turn-on-fci-mode))))
+ (erlang-mode . ((flycheck-erlang-include-path . ("../include"
+                                                  "../../include"
+                                                  "../../../include"))
+                 (flycheck-erlang-library-path . ("../../../_build/default/lib/APP_NAME"
+                                                  "../../../_build/default/lib/APP_NAME/ebin"))
+                 (erlang-indent-level          . 2))))
