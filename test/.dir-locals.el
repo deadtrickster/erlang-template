d10a2997576b120633e3c41796a95efe0f8c994a
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-20 12:15:40 +0300

    dir-locals for tests

---
+++ test/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil         . ((fill-column                  . 90)		 
+                 (eval                         . (turn-on-fci-mode))
+		 (flycheck-erlang-include-path . ("../../include"
+						  "../../../include"))
+                 (flycheck-erlang-library-path . ("../_build/default/lib/APP_NAME"
+                                                  "../_build/default/lib/APP_NAME/ebin")))))
