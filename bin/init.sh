3d73729c584abcd3fdd389a2235ee38112c138da
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-29 18:56:40 +0300

    replace origin address too

--- bin/init.sh
+++ bin/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$1/g"
+sed -i "s/erlang-template/$1/g" ./.git/config
 mv src/APP_NAME.app.src src/$1.app.src
 
