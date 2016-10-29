d7156bdf7089ff80ecc75163c2ef1242676dcf03
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-29 19:01:12 +0300

    initialize fresh git repository and commit instaniated template

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$1/g"
-sed -i "s/erlang-template/$1/g" ./.git/config
 mv src/APP_NAME.app.src src/$1.app.src
-
+rm -rf ./.git
+git init
+git remote add origin git@github.com:deadtrickster/$1.git
+rm ./bin/init.sh
+git add .
+git commit -m "initialized $1"
