4a53d3f1450b1d2cdc8d31c078cb50cf8f00c57e
Ilya Khaprov <dead.trickster@gmail.com>
2017-05-13 11:08:54 +0300

    increment version script

--- bin/init.sh
+++ bin/init.sh
@@ -3,6 +3,7 @@
 ELIXIR_MODULE_NAME=$(echo "$1" | sed 's/_\([a-z]\)/\U\1/g;s/^\([a-z]\)/\U\1/g')
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$1/g"
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/ELIXIR_MODULE/$ELIXIR_MODULE_NAME/g"
+sed -i "s/APP_NAME/$1/g" bin/increment-version
 mv src/APP_NAME.app.src src/$1.app.src
 mv lib/APP_NAME.ex lib/$1.ex
 rm -rf ./.git
