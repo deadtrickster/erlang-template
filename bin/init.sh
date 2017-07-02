48ddda233de71ca8845879948cc3301b32c45459
Ilya Khaprov <dead.trickster@gmail.com>
2017-07-02 10:40:42 +0300

    use current directory basename as default project name

--- bin/init.sh
+++ bin/init.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-ELIXIR_MODULE_NAME=$(echo "$1" | sed 's/_\([a-z]\)/\U\1/g;s/^\([a-z]\)/\U\1/g')
-find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$1/g"
+NAME=${1:-$(basename $(pwd))}
+ERLANG_MODULE_NAME=$(echo "$NAME" | sed 's/-/_/g')
+ELIXIR_MODULE_NAME=$(echo "$NAME" | sed 's/_\([a-z]\)/\U\1/g;s/^\([a-z]\)/\U\1/g')
+find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$ERLANG_MODULE_NAME/g"
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/ELIXIR_MODULE/$ELIXIR_MODULE_NAME/g"
-sed -i "s/APP_NAME/$1/g" bin/increment-version
-mv src/APP_NAME.app.src src/$1.app.src
-mv lib/APP_NAME.ex lib/$1.ex
+sed -i "s/APP_NAME/$ERLANG_MODULE_NAME/g" bin/increment-version
+mv src/APP_NAME.app.src src/$ERLANG_MODULE_NAME.app.src
+mv lib/APP_NAME.ex lib/$ERLANG_MODULE_NAME.ex
 rm -rf ./.git
 git init
-git remote add origin git@github.com:deadtrickster/$1.git
+git remote add origin git@github.com:deadtrickster/$NAME.git
 rm ./bin/init.sh
 ./rebar3 edoc
 git add .
 git commit -m "initialized $1"
+
