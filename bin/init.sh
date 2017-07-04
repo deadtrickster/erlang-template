168586351c59d625c693118322177a56bb16c109
Ilya Khaprov <dead.trickster@gmail.com>
2017-07-04 22:41:44 +0300

    Fix Elixir Module name generation

--- bin/init.sh
+++ bin/init.sh
@@ -2,7 +2,7 @@
 
 NAME=${1:-$(basename $(pwd))}
 ERLANG_MODULE_NAME=$(echo "$NAME" | sed 's/-/_/g')
-ELIXIR_MODULE_NAME=$(echo "$NAME" | sed 's/_\([a-z]\)/\U\1/g;s/^\([a-z]\)/\U\1/g')
+ELIXIR_MODULE_NAME=$(echo "$ERLANG_MODULE_NAME" | sed 's/_\([a-z]\)/\U\1/g;s/^\([a-z]\)/\U\1/g')
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/GIT_NAME/$NAME/g"
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/APP_NAME/$ERLANG_MODULE_NAME/g"
 find . \( -path ./bin -o -path ./.git \) -prune -o -type f -print0 | xargs -0 sed -i "s/ELIXIR_MODULE/$ELIXIR_MODULE_NAME/g"
