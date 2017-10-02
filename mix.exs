daa18fe5a3c3bec7958d85957fe45b59ea0f674b
Ilya Khaprov <dead.trickster@gmail.com>
2017-10-02 17:15:43 +0300

    mix.exs: fix and add credo/coveralls

--- mix.exs
+++ mix.exs
@@ -2,11 +2,15 @@ defmodule ELIXIR_MODULE.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :APP_NAME
+    [app: :APP_NAME,
      version: "0.0.1",
      deps: deps(),
      description: description(),
-     package: package()]
+     package: package(),
+     erlc_options: [:warnings_as_errors],
+     elixirc_options: [warnings_as_errors: true],
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.html": :test]]
   end
 
   defp description do
@@ -23,6 +27,7 @@ defmodule ELIXIR_MODULE.Mixfile do
   end
 
   defp deps do
-    []
+    [{:credo, "~> 0.8.7", only: [:dev, :test]},
+     {:excoveralls, "~> 0.7.3", only: [:test]}]
   end
 end
